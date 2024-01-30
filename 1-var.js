7fdfbf01a999466d7f2d3e6a1b966fb3cf33ad3d
Abuhuraira-Khan <abuhurairakhan3127@gmail.com>
2024-01-30 18:02:30 +0600

    js guide

---
+++ 1-var.js
@@ -0,0 +1,21 @@
+// Variables in  JavaScript
+
+console.log("Abu Huraira Khan");
+
+// var in js
+var num = 20;
+var num2 = 35;
+var total = num + num2;
+console.log(total);
+
+// let in js
+let bool = true;
+console.log(bool);
+let bool2 = false;
+console.log(bool2);
+
+// const in js
+const x = 10;
+const y = 6;
+
+
